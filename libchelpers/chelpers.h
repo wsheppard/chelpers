1ff57f73aaac5a4332d0ad6488d3dfd653b82cd2
Will Sheppard <wsheppard@embedded-bits.co.uk>
2020-09-22 21:33:53 +0100

    Additions

---
+++ libchelpers/chelpers.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <stdlib.h>
+#include <time.h>
+
+#define PACKED __attribute__ ((packed))
+#define CLEANUP __attribute__ ((__cleanup__(cleanup)))
+
+#define MEM_STR(p,n) ({\
+	static char __thread __msstorage[128];\
+	char *scur = __msstorage;\
+	const char *var = (const char*)(p);\
+	const char *end = var+(n);\
+	for( ; var<end; var++ )\
+	scur += sprintf(scur, "%02X", *var);\
+	*scur = 0;\
+	__msstorage;})
+
+#define MAC_STR(p) ({\
+	static char __thread outstr[19];\
+	char *scur = outstr;\
+	char *var = (char*)p;\
+	char *end = var+6;\
+	for( ; var<end; var++ )\
+	scur += sprintf(scur, "%02X:", *var);\
+	outstr[17] = 0;\
+	outstr;})
+
+int msleep(long msec);
